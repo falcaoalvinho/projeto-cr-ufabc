7ff9117e2f51b924fdcf26b65cb4b98d7ed75a85
Álvaro José <josealvaro3421@gmail.com>
2025-06-29 14:56:31 -0300

    Verificando se a registros que repetem mais de 2 vezes na tabela

--- moreinfo.py
+++ moreinfo.py
@@ -4,15 +4,20 @@ url = input('Insira a URL do repositório: ')
 response = requests.get(url)
 data = response.json()
 
+jogadores= []
+
 repetidos = []
 id_repetidos = []
-jogadores = []
+
+repeticoes_jogadores = {}
 
 data_list = list(data['transferências'])
 
+
+
 print('\nLISTA DE TIMES:')
 for registro in data_list:
-    print(f'{registro['jogador']}')
+    # print(f'{registro['jogador']}')
 
     if registro['jogador'] in jogadores:
         repetidos.append(registro['jogador'])
@@ -20,9 +25,21 @@ for registro in data_list:
 
     jogadores.append(registro['jogador'])
 
-print('\nJOGADORES REPETIDOS: ')
-print(', '.join(repetidos))
+set(jogadores)
+set(repetidos)
+set(id_repetidos)
+list((jogadores, repetidos, id_repetidos))
 
-print('\nIDs DOS JOGADORES REPETIDOS')
-print(', '.join(id_repetidos))
+for jogador in jogadores:
+    repeticoes_jogadores[str(jogador)] = jogadores.count(jogador)
 
+# print('\nJOGADORES REPETIDOS: ')
+# print(', '.join(repetidos))
+
+# print('\nIDs DOS JOGADORES REPETIDOS')
+# print(', '.join(id_repetidos))
+
+print('\nREPETIÇÕES DE CADA JOGADOR: ')
+for jogador, repeticoes in repeticoes_jogadores.items():
+    if repeticoes > 1:
+        print(f'{jogador}: {repeticoes}')
