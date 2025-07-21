bf6e0d2ca8a59289f40880cf521e0bf6a093c43d
Álvaro José <josealvaro3421@gmail.com>
2025-07-20 23:28:28 -0300

    adicionado código para coleta, ordenamento e impressão das centralidades de entrada e saída de cada um dos vértices do grafo

--- centralidades_times.py
+++ centralidades_times.py
@@ -6,6 +6,7 @@
 '''
 # Import do networkx
 import networkx as nx
+import os
 
 # Import da função que usamos para construir o grafo, e instanciamento de um modelo
 from grafo import montar_grafo
@@ -16,35 +17,59 @@ cent_vertices       = nx.degree_centrality(grafo)
 cent_proximidade    = nx.closeness_centrality(grafo)
 cent_intermediacao  = nx.betweenness_centrality(grafo)
 cent_autovetor      = nx.eigenvector_centrality(grafo)
+cent_out_dregree    = nx.out_degree_centrality(grafo)
+cent_in_dregree     = nx.in_degree_centrality(grafo)
 
 # Ordenamento dos valores encontrados
-cent_vertices       = dict(sorted(nx.degree_centrality(grafo).items(), key=lambda item: item[1], reverse=True ))
-cent_proximidade    = dict(sorted(nx.closeness_centrality(grafo).items(), key=lambda item: item[1], reverse=True ))
-cent_intermediacao  = dict(sorted(nx.betweenness_centrality(grafo).items(), key=lambda item: item[1], reverse=True ))
-cent_autovetor      = dict(sorted(nx.eigenvector_centrality(grafo).items(), key=lambda item: item[1], reverse=True ))
+cent_vertices       = dict(sorted(cent_vertices.items(), key=lambda item: item[1], reverse=True ))
+cent_proximidade    = dict(sorted(cent_proximidade.items(), key=lambda item: item[1], reverse=True ))
+cent_intermediacao  = dict(sorted(cent_intermediacao.items(), key=lambda item: item[1], reverse=True ))
+cent_autovetor      = dict(sorted(cent_autovetor.items(), key=lambda item: item[1], reverse=True ))
+cent_out_dregree    = dict(sorted(cent_out_dregree.items(), key=lambda item: item[1], reverse=True))
+cent_in_dregree     = dict(sorted(cent_in_dregree.items(), key=lambda item: item[1], reverse=True))
 
-print('CENTRALIDADE: VÉRTICES')
+# Print dos valores para cada um dos casos usando loops de chave, valor
+print('\nCENTRALIDADE: VÉRTICES (NODE)')
 for chave, valor in cent_vertices.items():
     print(f'{chave} : {valor}')
 
 input('\nAPERTE [ENTER] PARA CONTINUAR...')
+os.system('cls')
 
 print('\nCENTRALIDADE: PROXIMIDADE (CLOSENESS)')
 for chave, valor in cent_proximidade.items():
     print(f'{chave} : {valor}')
 
 input('\nAPERTE [ENTER] PARA CONTINUAR...')
+os.system('cls')
 
 print('\nCENTRALIDADE: INTERMEDIAÇÃO (BETWEENNESS)')
 for chave, valor in cent_intermediacao.items():
     print(f'{chave} : {valor}')
 
 input('\nAPERTE [ENTER] PARA CONTINUAR...')
+os.system('cls')
 
 print('\nCENTRALIDADE: AUTOVETOR (EIGENVECTOR)')
 for chave, valor in cent_autovetor.items():
     print(f'{chave} : {valor}')
 
+input('\nAPERTE [ENTER] PARA CONTINUAR...')
+os.system('cls')
+
+print('\nCENTRALIDADE: GRAU DE SAÍDA (OUT DEGREE)')
+for chave, valor in cent_autovetor.items():
+    print(f'{chave} : {valor}')
+
+input('\nAPERTE [ENTER] PARA CONTINUAR...')
+os.system('cls')
+
+print('\nCENTRALIDADE: GRAU DE ENTRADA (IN DEGREE)')
+for chave, valor in cent_autovetor.items():
+    print(f'{chave} : {valor}')
+
+input('\nEXECUÇÃO FINALIZADA COM SUSCESSO, APETE [ENTER] PARA ENCERRAR...')
+os.system('cls')
 
 '''
     OBSERVAÇÕES:
